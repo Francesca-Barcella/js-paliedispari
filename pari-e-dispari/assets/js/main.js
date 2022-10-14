045504170f9df6e1de0083ee35e2bf8f61897293
Francesca-Barcella <pollon.fra@gmail>
2022-10-14 14:55:29 +0200

    JS - PD - userNumber + numberPc

--- pari-e-dispari/assets/js/main.js
+++ pari-e-dispari/assets/js/main.js
@@ -1,14 +1,21 @@
 //alert('ciao, sei nella cartella pari e dispari!')
 
-/* 
+/************************************************** 
 TRACCIA 2
 Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 
-*/
+**************************************************/
 
 // - number(prompt) per chiedere un numero all'utente
 // - numero random per il pc (math.random)
 // - faccio la somma dei numeri per vedere se è pari o dispari (% 2 == 0)
 // - creo la (function) con gli (if else)
 // - console.log del vincitore
+
+const numberPlayer = Number(prompt('Sceglli un numero da 1 a 5'));
+console.log('il tuo numero è ' + numberPlayer);
+
+const numberPc = Math.floor(Math.random() * 5 + 1);
+console.log('il tuo numero del computer è ' + numberPc);
+
