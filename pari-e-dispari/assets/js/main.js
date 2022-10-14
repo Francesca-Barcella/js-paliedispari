79b4a001a068adf4ee387e2a51ea09a1cebc4f6f
Francesca-Barcella <pollon.fra@gmail>
2022-10-14 15:22:29 +0200

    JS - PD -  function + stamp results

--- pari-e-dispari/assets/js/main.js
+++ pari-e-dispari/assets/js/main.js
@@ -7,15 +7,35 @@ L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un n
 
 **************************************************/
 
-// - number(prompt) per chiedere un numero all'utente
-// - numero random per il pc (math.random)
-// - faccio la somma dei numeri per vedere se è pari o dispari (% 2 == 0)
-// - creo la (function) con gli (if else)
-// - console.log del vincitore
+//1 - number(prompt) per chiedere un numero all'utente
+//2 - numero random per il pc (math.random)
+//3 - faccio la somma dei numeri per vedere se è pari o dispari (% 2 == 0)
+//4 - creo la (function) con gli (if else)
+//5 - console.log del vincitore
 
-const numberPlayer = Number(prompt('Sceglli un numero da 1 a 5'));
-console.log('il tuo numero è ' + numberPlayer);
 
+// 1 - number(prompt) per chiedere un numero all'utente
+const pariDispariUser = prompt('Scegli pari o dispari');
+console.log('hai scelto: ' + pariDispariUser);
+const numberPlayer = Number(prompt('Scegli un numero da 1 a 5'));
+console.log('hai scelto il numero: ' + numberPlayer);
+
+//2 - numero random per il pc (math.random)
 const numberPc = Math.floor(Math.random() * 5 + 1);
-console.log('il tuo numero del computer è ' + numberPc);
+console.log('Il numero del computer è: ' + numberPc);
+
+//3 - faccio la somma dei numeri per vedere se è pari o dispari (% 2 == 0)
+const sum = numberPlayer + numberPc;
+console.log('la somma dei due numeri è: ' + sum);
+
+function pariDispari(number) {
+    if ((number % 2 !== 0) && (pariDispariUser % 2 !== 0)) {
+        console.log('la somma è pari ed hai vinto tu');
+    } else {
+        console.log('la somma è dispari ed ha vinto il computer');
+    }
+}
+
+pariDispari();
+
 
