eb5abd7ecc9e385cc2b292190cf157b8668b6acd
Francesca-Barcella <pollon.fra@gmail>
2022-10-14 12:04:21 +0200

    JS PL - analisi traccia 2

--- pari-e-dispari/assets/js/main.js
+++ pari-e-dispari/assets/js/main.js
@@ -1,1 +1,14 @@
-alert('ciao, sei nella cartella pari e dispari!')
+//alert('ciao, sei nella cartella pari e dispari!')
+
+/* 
+TRACCIA 2
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+
+*/
+
+// - number(prompt) per chiedere un numero all'utente
+// - numero random per il pc (math.random)
+// - faccio la somma dei numeri per vedere se è pari o dispari (% 2 == 0)
+// - creo la (function) con gli (if else)
+// - console.log del vincitore
