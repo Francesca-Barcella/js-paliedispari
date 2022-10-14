aa6cb90d9a8c2ec38c07d0f23c70c254b6bbedcd
Francesca-Barcella <pollon.fra@gmail>
2022-10-14 12:13:36 +0200

    JS  Palindormo - analisi in  italiano

--- palindromo/assets/js/main.js
+++ palindromo/assets/js/main.js
@@ -1,1 +1,13 @@
-alert('ciao, sei nella cartellapa lindromo!')
+//alert('ciao, sei nella cartella palindromo!')
+
+/**********************************************************************************
+TRACCIA 1 
+Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+***********************************************************************************/
+
+// - (prompt) per chiedere all'utente una parola
+// - trovare il modo di scriverle al contrario
+// - trovare il modo di dire true se si legge al contrario
+// - inseriamo quello che serve nella (function)
+// - console.log positivo o negativo
