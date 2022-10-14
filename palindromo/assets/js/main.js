ed4674943cdcda39b29e18c2b726d64bcee23800
Francesca-Barcella <pollon.fra@gmail>
2022-10-14 13:26:37 +0200

    PS P - blocco if else

--- palindromo/assets/js/main.js
+++ palindromo/assets/js/main.js
@@ -12,9 +12,11 @@ Chiedere all’utente di inserire una parola Creare una funzione per capire se l
 // 4 - inseriamo quello che serve nella (function)
 // 5 - console.log positivo o negativo
 
+// 1 - (prompt) per chiedere all'utente una parola
 const userWord = prompt('scrivi una parola');
 console.log(userWord);
 
+// 2 - trovare il modo di scriverle al contrario (fare array con reverse - split - join) 
 const splitWord = userWord.split('');
 console.log(splitWord);
 
@@ -24,7 +26,12 @@ console.log(reverseLetters);
 const reverseUserWord = reverseLetters.join('');
 console.log(reverseUserWord);
 
-
-
+// 5 - console.log positivo o negativo
+// 3 - trovare il modo di dire true se si legge al contrario (fare array con reverse - split - join)
+if (userWord == reverseUserWord){
+    console.log('la tu parola è palindroma');
+} else {
+    console.log('la tua parola non è palindroma');
+}
 
 
